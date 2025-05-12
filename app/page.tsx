a34da7c2880cfcf158b14fbfbd6cc41c3a0973f6
3S Cavalcante <aderc19@gmail.com>
2025-05-11 21:28:32 -0300

    :sparkles: feat: Início da seção Hero.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Header from './components/landing-page/Header';
+import Hero from './components/landing-page/Hero';
+
 export default function Home() {
-  return <div className="bg-background-primary">Hello World</div>;
+  return (
+    <div className="max-w-7xl mx-auto border">
+      <div className="text-white">Hello World</div>
+      <Header />
+      <Hero />
+    </div>
+  );
 }
