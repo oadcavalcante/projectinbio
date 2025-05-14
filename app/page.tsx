741ccb19bd6fc7e6079e74fca1b1d2aeb2e7a234
3S Cavalcante <aderc19@gmail.com>
2025-05-13 21:14:53 -0300

    :recycle: refactor: ajustes nas nomenclaturas

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from './components/landing-page/Hero';
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto border">
-      <div className="text-white">Hello World</div>
       <Header />
       <Hero />
     </div>
